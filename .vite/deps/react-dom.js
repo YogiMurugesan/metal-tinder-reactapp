a0f73c485694455d0713c5ee1ece439d4a04832e
Iniya737 <iniya737@gmail.com>
2026-09-21 17:40:17 +0530

    Restructure project into frontend and backend

--- .vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-3Z4C5OF4.js";
-import "./chunk-Y7MCAJS7.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
--- src/components/Progress.jsx
+++
@@ -1,9 +0,0 @@
-function Progress({ value }) {
-  return (
-    <div className="progress">
-      <span style={{ width: `${value * 100}%` }} />
-    </div>
-  );
-}
-
-export default Progress;
